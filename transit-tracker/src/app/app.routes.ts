c8a64980c03f10e5417b3455a4574eb7448b9e5f
Klas <klas.nederman@gmail.com>
2025-07-10 21:22:52 +0200

    moved files

--- transit-tracker/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import {RouteViewerComponent} from './route-viewer/route-viewer';
-export const routes: Routes = [
-  {
-    path: 'routes/:shapeId',
-    component: RouteViewerComponent,
-  }
-];
